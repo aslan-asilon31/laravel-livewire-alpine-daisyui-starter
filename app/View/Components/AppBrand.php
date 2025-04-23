2607167ccbba399bdcd285a19b106c0b2d0bad55
aslan-asilon31 <aslanasilon31@gmail.com>
2025-04-23 16:59:25 +0700

    update

--- app/View/Components/AppBrand.php
+++ app/View/Components/AppBrand.php
@@ -26,14 +26,14 @@ class AppBrand extends Component
                     <!-- Hidden when collapsed -->
                     <div {{ $attributes->class(["hidden-when-collapsed"]) }}>
                         <div class="flex items-center justify-center ">
-                            <img src="{{asset('umeda-logo.png')}}" class="w-24 md:w-32  -mb-1" alt="" srcset="">
+                            <img src="{{asset('logo-no-brand.png')}}" class="w-24 md:w-32  -mb-1" alt="" srcset="">
                         </div>
                     </div>
 
                     <!-- Display when collapsed -->
                     <div class="display-when-collapsed hidden mx-5 mt-4 lg:mb-6 h-[28px]">
                         <div class="flex items-center gap-2 text-center ">
-                            <img src="{{asset('umeda-logo.png')}}" class="w-16 md:w-24  -mb-1" alt="" srcset="">
+                            <img src="{{asset('logo-no-brand.png')}}" class="w-16 md:w-24  -mb-1" alt="" srcset="">
                         </div>
                     </div>
                 </a>
